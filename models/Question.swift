349d2d1eee484b761bc75f3c169489ad831cfee2
Samuel Amante <amantsa76@gmail.com>
2025-01-07 10:08:50 -0600

    added first two structs

---
+++ models/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Flag Game Organized
+//
+//  Created by Samuel Amante on 1/7/25.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswer: [Answer]
+}
+    
