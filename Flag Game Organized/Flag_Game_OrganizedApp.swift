6046b76df3f7bb656d8e98162e05e423c8be3bb1
Samuel Amante <amantsa76@gmail.com>
2025-01-07 09:52:29 -0600

    Initial Commit

---
+++ Flag Game Organized/Flag_Game_OrganizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_Game_OrganizedApp.swift
+//  Flag Game Organized
+//
+//  Created by Samuel Amante on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Flag_Game_OrganizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
